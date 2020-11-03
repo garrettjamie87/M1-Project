fbce8f7de9939fbbbed67e90661d99e74b41ac1c
Jamie Garrett <garrettjamie87@googlemail.com>
2020-11-03 16:52:41 +0100

    fetch and display data from api

--- JS/login.js
+++ JS/login.js
@@ -61,7 +61,7 @@ class Login {
 
 const login = new Login();
 
-window.addEventListener('load', function () {
+window.addEventListener("load", function () {
 login.loginButton.addEventListener('click', login.handleSubmit );
 
-})
+});
--- JS/wookiepediaAPI.js
+++ JS/wookiepediaAPI.js
@@ -1,35 +1,155 @@
 'use strict'
 
-const starWarsList = document.querySelectorAll('.starwars-list');
+getFirstPosition();
+
+async function getFirstPosition() {
+      const epNum = document.querySelector(".first-position");
+     try {
+       // storing the response fetching the data
+       const response = await fetch(
+         'https://swapi.dev/api/films/'
+       );
+       console.log("response", response);
+       // storing data in form of a json file
+       const dataAPI = await response.json();
+       const wookie = dataAPI.results[0]
+       console.log(wookie);
+       const createSpan= document.createElement('div')
+       createSpan.innerHTML=`<p>${wookie.director}</p>
+       <p>${wookie.release_date}</p>
+       `
+       epNum.appendChild(createSpan)
+       console.log("data", dataAPI);
+     } catch (err) {
+       console.log(err);
+     }
+   }
+//************************************* */
 
 
-function getAllData(){
+getSecondPosition();
+   async function getSecondPosition() {
+      const epNum = document.querySelector(".second-position");
+     try {
+       // storing the response fetching the data
+       const response = await fetch(
+         'https://swapi.dev/api/films/'
+       );
+       console.log("response", response);
+       // storing data in form of a json file
+       const dataAPI = await response.json();
+       const wookie = dataAPI.results[1]
+       console.log(wookie);
+       const createSpan= document.createElement('div')
+       createSpan.innerHTML=`<p>${wookie.director}</p>
+       <p>${wookie.release_date}</p>
+       `
+       epNum.appendChild(createSpan)
+       console.log("data", dataAPI);
+     } catch (err) {
+       console.log(err);
+     }
+   }
+//************************************* */
 
-      fetch('https://swapi.dev/api/films/1')
-      .then((response) => {
-            console.log(response);
+     getThirdPosition();
+     async function getThirdPosition() {
+      const epNum = document.querySelector(".third-position");
+      try {
+       // storing the response fetching the data
+       const response = await fetch(
+         'https://swapi.dev/api/films/'
+       );
+       console.log("response", response);
+       // storing data in form of a json file
+       const dataAPI = await response.json();
+       const wookie = dataAPI.results[2]
+       console.log(wookie);
+       const createSpan= document.createElement('div')
+       createSpan.innerHTML=`<p>${wookie.director}</p>
+       <p>${wookie.release_date}</p>
+       `
+       epNum.appendChild(createSpan)
+       console.log("data", dataAPI);
+     } catch (err) {
+       console.log(err);
+     }
+   }
+   
+   //************************************* */
 
-            return response.json();
-      })
-      .then((data) => {
-      
-            console.log('data', data)
-      })
-      .catch((err)=>{})
+   getFourthPosition();
+   async function getFourthPosition() {
+    const epNum = document.querySelector(".fourth-position");
+    try {
+     // storing the response fetching the data
+     const response = await fetch(
+       'https://swapi.dev/api/films/'
+     );
+     console.log("response", response);
+     // storing data in form of a json file
+     const dataAPI = await response.json();
+     const wookie = dataAPI.results[3]
+     console.log(wookie);
+     const createSpan= document.createElement('div')
+     createSpan.innerHTML=`<p>${wookie.director}</p>
+     <p>${wookie.release_date}</p>
+     `
+     epNum.appendChild(createSpan)
+     console.log("data", dataAPI);
+   } catch (err) {
+     console.log(err);
+   }
+ }
+ 
+ //************************************* */
+
+ getFifthPosition();
+ async function getFifthPosition() {
+  const epNum = document.querySelector(".fifth-position");
+  try {
+   // storing the response fetching the data
+   const response = await fetch(
+     'https://swapi.dev/api/films/'
+   );
+   console.log("response", response);
+   // storing data in form of a json file
+   const dataAPI = await response.json();
+   const wookie = dataAPI.results[4]
+   console.log(wookie);
+   const createSpan= document.createElement('div')
+   createSpan.innerHTML=`<p>${wookie.director}</p>
+   <p>${wookie.release_date}</p>
+   `
+   epNum.appendChild(createSpan)
+   console.log("data", dataAPI);
+ } catch (err) {
+   console.log(err);
+ }
 }
-getAllData();
 
-function display() {
+ //************************************* */
 
-      fetch('https://swapi.dev/api/films/0')
-      .then((response) => {
-            console.log(response);
-
-            return response.json();
-      })
-      .then((data) => {
-      
-            console.log('data', data)
-      })
-      .catch((err)=>{})
+ getsixthPosition();
+ async function getsixthPosition() {
+  const epNum = document.querySelector(".sixth-position");
+  try {
+   // storing the response fetching the data
+   const response = await fetch(
+     'https://swapi.dev/api/films/'
+   );
+   console.log("response", response);
+   // storing data in form of a json file
+   const dataAPI = await response.json();
+   const wookie = dataAPI.results[5]
+   console.log(wookie);
+   const createSpan= document.createElement('div')
+   createSpan.innerHTML=`<p>${wookie.director}</p>
+   <p>${wookie.release_date}</p>
+   `
+   epNum.appendChild(createSpan)
+   console.log("data", dataAPI);
+ } catch (err) {
+   console.log(err);
+ }
 }
